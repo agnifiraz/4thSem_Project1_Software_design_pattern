51264f86f8a985ba0e3ebf70e160698b1f817327
AGNITA PAUL <agnifiraz@gmail.com>
2022-05-24 21:56:55 -0400

    Add project files.

---
+++ Director/Program.cs
@@ -0,0 +1,123 @@
+﻿using DocumentFactory;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Director
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string doctype = "";
+            string[] commands;
+            var list = File.ReadAllText("Test.txt");
+            commands = list.Split('#');
+
+            foreach (var command in commands)
+            {
+                count++;
+                var strippedCommand = Regex.Replace(command, @"\t|\n|\r", "");
+                var commandList = strippedCommand.Split(':', ';');
+                if (count == 1)
+                {
+                    doctype = commandList[1];
+                }
+                switch (doctype)
+                {
+                    case "Markdown":
+                        switch (commandList[0])
+                        {
+                            case "Document":
+                                MarkDownFactory.GetInstance();
+                                MarkDownFactory.markDownFactory.CreateDocument(commandList[2]);
+                                break;
+                            case "Element":
+                                //MarkDownFactory.GetInstance();
+                                //MarkDownFactory.markDownFactory.CreateElement(commandList[1], commandList[3]);
+                                break;
+                            case "Run":
+                                count = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case "Html":
+                        switch (commandList[0])
+                        {
+                            case "Document":
+                                HTMLFactory.GetInstance();
+                                HTMLFactory.htmlFactory.CreateDocument(commandList[2]);
+                                break;
+                            case "Element":
+                                //HTMLFactory.GetInstance();
+                                HTMLFactory.htmlFactory.CreateElement(commandList[1], commandList[3]);
+                                break;
+                            case "Run":
+                                HTMLDocument.GetInstance();
+                                HTMLDocument.htmlDocument.RunDocument();
+                                count = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace Director
+//{
+//    class Program
+//    {
+
+//        static void Main(string[] args)
+//        {
+//            string[] commands;
+//            var list = File.ReadAllText("CreateDocumentScript.txt");
+//            commands = list.Split('#');
+
+//            foreach (var command in commands)
+//            {
+//                var strippedCommand = Regex.Replace(command, @"\t|\n|\r", "");
+//                var commandList = strippedCommand.Split(':');
+//                switch (commandList[0])
+//                {
+//                    case "Document":
+//                        // Your document creation code goes here
+//                        break;
+//                    case "Element":
+//                        // Your element creation code goes here
+//                        break;
+//                    case "Run":
+//                        // Your document running code goes here
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ DocumentFactory/IDocumentFactory.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DocumentFactory
+{
+    public interface IDocument
+    {
+        void RunDocument();
+        void AddElement(IElement newElement);
+    }
+    public interface IElement { }
+    public interface IDocumentFactory
+    {
+        IDocument CreateDocument(string fileName);
+        IElement CreateElement(string elementType, string props);
+    }
+
+    public class HTMLElement : IElement { }
+    public class HTMLHeaderElement : HTMLElement
+    {
+        private string theData;
+        public HTMLHeaderElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            int i = 1;
+            var temp = theData.Split(';');
+            return $"<h{i}>" + temp[1] + $"</h{i}>";
+        }
+    }
+
+    public class HTMLImageElement : HTMLElement
+    {
+        private string theData;
+        public HTMLImageElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return "Image";
+        }
+    }
+
+    public class HTMLListElement : HTMLElement
+    {
+        private string theData;
+        public HTMLListElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return "List";
+        }
+    }
+    public class HTMLTableElement : HTMLElement
+    {
+        private string theData;
+        public HTMLTableElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return "Table";
+        }
+    }
+    public class HTMLFactory : IDocumentFactory
+    {
+        public static HTMLFactory htmlFactory;
+        HTMLDocument htmlDocument;
+        public IDocument CreateDocument(string fileName)
+        {
+            htmlDocument = new HTMLDocument(fileName);
+            File.Create(fileName);
+            return htmlDocument;
+        }
+        public IElement CreateElement(string elementType, string data)
+        {
+            IElement element = null;
+            switch (elementType)
+            {
+                case "Header":
+                    element = new HTMLHeaderElement(data);
+                    break;
+                case "Image":
+                    element = new HTMLImageElement(data);
+                    break;
+                case "List":
+                    element = new HTMLListElement(data);
+                    break;
+                case "Table":
+                    element = new HTMLTableElement(data);
+                    break;
+                default:
+                    break;
+            }
+            htmlDocument.AddElement(element);
+            return element;
+        }
+
+        private HTMLFactory()
+        {
+
+        }
+
+        public static HTMLFactory GetInstance()
+        {
+            if (htmlFactory == null)
+            {
+                htmlFactory = new HTMLFactory();
+            }
+            return htmlFactory;
+        }
+    }
+    public class HTMLDocument : IDocument
+    {
+        public static HTMLDocument htmlDocument;
+        private string fileName;
+        List<HTMLElement> content = new List<HTMLElement>();
+        string allContent = "";
+        public HTMLDocument(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public HTMLDocument()
+        {
+        }
+
+        public void AddElement(IElement newElement)
+        {
+            var tempEl = (HTMLElement)newElement;
+            content.Add(tempEl);
+        }
+        public void RunDocument()
+        {
+            for (int i = 0; i < content.Count; i++)
+            {
+                allContent = allContent + content[i].ToString() + "\n";
+            }
+            File.WriteAllText(fileName, allContent);
+            System.Diagnostics.Process.Start("chrome.exe", fileName);
+        }
+        public static HTMLDocument GetInstance()
+        {
+            if (htmlDocument == null)
+            {
+                htmlDocument = new HTMLDocument();
+            }
+            return htmlDocument;
+        }
+    }
+    public class MarkDownElement : IElement { }
+    public class MarkDownHeaderElement : MarkDownElement
+    {
+        private string theData;
+        public MarkDownHeaderElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return $"<h{temp[0]}>" + theData[1] + $"</h{temp[0]}>";
+        }
+    }
+
+    public class MarkDownImageElement : MarkDownElement
+    {
+        private string theData;
+        public MarkDownImageElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return "Image";
+        }
+    }
+
+    public class MarkDownListElement : MarkDownElement
+    {
+        private string theData;
+        public MarkDownListElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return "List";
+        }
+    }
+    public class MarkDownTableElement : MarkDownElement
+    {
+        private string theData;
+        public MarkDownTableElement(string tempData)
+        {
+            theData = tempData;
+        }
+        public override string ToString()
+        {
+            var temp = theData.Split(';');
+            return "Table";
+        }
+    }
+    public class MarkDownFactory : IDocumentFactory
+    {
+        public static MarkDownFactory markDownFactory;
+        MarkDownDocument markDownDocument;
+        public IDocument CreateDocument(string fileName)
+        {
+            markDownDocument = new MarkDownDocument(fileName);
+            File.Create(fileName);
+            return markDownDocument;
+        }
+        public IElement CreateElement(string elementType, string data)
+        {
+            IElement element = null;
+
+            switch (elementType)
+            {
+                case "Header":
+                    element = new MarkDownHeaderElement(data);
+                    break;
+                case "Image":
+                    element = new MarkDownImageElement(data);
+                    break;
+                case "List":
+                    element = new MarkDownListElement(data);
+                    break;
+                case "Table":
+                    element = new MarkDownTableElement(data);
+                    break;
+                default:
+                    break;
+            }
+            markDownDocument.AddElement(element);
+            return element;
+        }
+
+        private MarkDownFactory()
+        {
+
+        }
+
+        public static MarkDownFactory GetInstance()
+        {
+            if (markDownFactory == null)
+            {
+                markDownFactory = new MarkDownFactory();
+            }
+            return markDownFactory;
+        }
+    }
+    public class MarkDownDocument : IDocument
+    {
+        private string fileName;
+        List<MarkDownElement> content = new List<MarkDownElement>();
+        string allContent = "";
+        public MarkDownDocument(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void RunDocument()
+        {
+            for (int i = 0; i < content.Count; i++)
+            {
+                allContent = allContent + content[i].ToString() + "\n";
+            }
+            File.WriteAllText(fileName, allContent);
+            System.Diagnostics.Process.Start("chrome.exe", fileName);
+        }
+        public void AddElement(IElement newElement)
+        {
+            //var tempData = (MarkDownElement)newElement;
+            content.Add((MarkDownElement)newElement);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
